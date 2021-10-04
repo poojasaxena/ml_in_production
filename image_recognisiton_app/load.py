80e9e8b229440d52f9ea485eded2944dd006eab6
Pooja SAXENA <nrjrasaxena@gmail.com>
2021-10-04 13:02:17 +0200

    working with output showing image/confidence

--- image_recognisiton_app/load.py
+++ image_recognisiton_app/load.py
@@ -1,29 +1,20 @@
-import numpy as np
-import tensorflow.keras.models
-from tensorflow.keras.models import model_from_json
+"""
+to load the pretrained model and json.
+"""
 import tensorflow as tf
-from tensorflow.keras import backend
-
+from tensorflow.keras.models import model_from_json
 
 def init():
-    json_file = open('model/model.json','r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    #load weights into new model
+    """Return the loaded model and graph"""
+    try:
+        with open('model/model.json','r', encoding="utf-8") as f_json:
+            json = f_json.read()
+    except OSError:
+        print("model.json is not found")
+    loaded_model=model_from_json(json)
     loaded_model.load_weights("model/model.h5")
-    print("Loaded Model from disk")
-
-    #compile and evaluate loaded model
+    print("Weights are loaded from disk")
     loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-    #loss,accuracy = model.evaluate(X_test,y_test)
-    #print('loss:', loss)
-    #print('accuracy:', accuracy)
-    #out = loaded_model.predict(img)
-    #print(out)
-    #print(class_names[np.argmax(out)])
-    # convert the response to a string
-    #response = class_names[np.argmax(out)]
-    #return str(response)
+    #loaded_model.run_eagerly=True
     graph = tf.compat.v1.get_default_graph()
-    return loaded_model,graph
+    return loaded_model, graph
