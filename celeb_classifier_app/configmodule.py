b025ba71cd497c52447f65c44d5e501ebf7ab0fa
Pooja SAXENA <nrjrasaxena@gmail.com>
2021-11-23 10:10:21 +0100

    added variable:LOCAL_DATASET_DIR

--- celeb_classifier_app/configmodule.py
+++ celeb_classifier_app/configmodule.py
@@ -1,19 +1,19 @@
 import os
 
 class Config(object):
-    HOST_NAME      = "0.0.0.0"
-    PORT_NUMBER    = 8000
-    DEBUG_STATE    = 'False'
-    SAVED_MODEL_DIR = os.environ.get('DirForSavedModel') + '/face_recognisition/'
-    
+    HOST_NAME             = "0.0.0.0"
+    PORT_NUMBER           = 8000
+    DEBUG_STATE           = 'False'
+    SAVED_MODEL_DIR       = os.environ.get('DirForSavedModel') + '/face_recognisition/'
+    LOCAL_DATASET_DIR     = os.environ.get('DirForLocalDataset') + '/6-celebrity-faces-dataset/'
+    MODEL_CLASSIFICATION  = "svm_classifie_6_celebraties.pkl"    
+
 class Developement(Config):
     MODEL_EMBEDDING       = "facenet_keras.h5"
-    MODEL_CLASSIFICATION  = "svm_classifie_5_celebraties.pkl"
-    THRESHOLD        = 50
+    THRESHOLD             = 50
 
 class Production(Config):
     MODEL_EMBEDDING       = "facenet_keras.h5"
-    MODEL_CLASSIFICATION  = "svm_classifie_5_celebraties.pkl"
-    THRESHOLD        = 90
+    THRESHOLD             = 90
 
     
