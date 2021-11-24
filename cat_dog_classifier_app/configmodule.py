11d43aaf368279cf7b8c61519517b46349a2837b
Pooja SAXENA <nrjrasaxena@gmail.com>
2021-11-24 13:41:28 +0100

    added 95% transferlearning model

--- cat_dog_classifier_app/configmodule.py
+++ cat_dog_classifier_app/configmodule.py
@@ -1,15 +1,15 @@
 import os
 
 class Config(object):
-    LOSS_FUNCTION  = "categorical_crossentropy"
-    OPTIMIZER      = "adam"
-    HOST_NAME      = "0.0.0.0"
-    PORT_NUMBER    = 8000
-    DEBUG_STATE    = 'False'
-    CLASSES_NAME   = ['dog','cat']
-    MAIL_USERNAME  = os.environ.get('MAIL_USERNAME')
-    IMAGE_SIZE     = 200
+    LOSS_FUNCTION   = "categorical_crossentropy"
+    OPTIMIZER       = "adam"
+    HOST_NAME       = "0.0.0.0"
+    PORT_NUMBER     = 5000
+    DEBUG_STATE     = 'True'
+    CLASSES_NAME    = ['dog','cat']
+    IMAGE_SIZE      = 200
     SAVED_MODEL_DIR = os.environ.get('DirForSavedModel')
+    MAIL_USERNAME   = os.environ.get('MAIL_USERNAME')
     
 class CnnConfig(Config):
     MODEL_JSON     = "model_dogcat_classifier_cnn_85acc_okt21.json"
@@ -24,5 +24,11 @@ class AugmentationConfig(Config):
 class TransferLearningConfig(Config):
     MODEL_JSON     = "model_dogcat_classifier_vgg_tLearn_93acc_okt21.json"
     MODEL_H5       = "model_dogcat_classifier_vgg_tLearn_93acc_okt21.h5"
+    IMAGE_SIZE     = 224
+    THRESHOLD      = 80
+
+class TransferLearningConfig95(Config):
+    MODEL_JSON     = "model_dogcat_classifier_vgg_tLearn_95acc_okt21.json"
+    MODEL_H5       = "model_dogcat_classifier_vgg_tLearn_95acc_okt21.h5"
     IMAGE_SIZE     = 224
     THRESHOLD      = 80
